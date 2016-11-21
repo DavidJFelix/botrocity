67531300b6247e1de8d60056e95015a5e0b3cae6
David J Felix <felix.davidj@gmail.com>
2016-11-21 06:21:51 -0500

    Begin adding giphy support

---
+++ modules/giphy/search.go
@@ -0,0 +1,14 @@
+package giphy
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func searchGiphyAPI() {
+}
+
+// HandleGiphySearchText will respond
+func HandleGiphySearchText(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+}
