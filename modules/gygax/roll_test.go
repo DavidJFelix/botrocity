d026c55b679d47a6115b6b50354956307046ba01
David J Felix <felix.davidj@gmail.com>
2016-11-21 15:32:37 -0500

    Work on tests a tidbit

--- modules/gygax/roll_test.go
+++ modules/gygax/roll_test.go
@@ -8,6 +8,10 @@ import (
 
 func TestParseTextForDice(t *testing.T) {
   t.Parallel()
+  if !()
+  if !(reflect.DeepEqual(parseTextForDice("d10 d20"), []int{10, 20})) {
+    t.Fail()
+  }
   if !(reflect.DeepEqual(parseTextForDice("d4"), []int{4})) {
     t.Fail()
   }
